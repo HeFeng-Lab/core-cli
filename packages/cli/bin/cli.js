632964e4cf0be3e42fd16e82aaa078acb2993384
hefeng6500 <2443992009@qq.com>
2023-12-01 13:28:19 +0800

    feat: init

---
+++ packages/cli/bin/cli.js
@@ -0,0 +1,17 @@
+#!/usr/bin/env node
+
+import importLocal from "import-local"
+import { dirname, filename } from "dirname-filename-esm"
+import log from "npmlog"
+import entry from "../lib/index.js"
+
+const __dirname = dirname(import.meta)
+const __filename = filename(import.meta)
+
+if (importLocal(__filename)) {
+  log.info("core-cli", "Using local version of this package")
+} else {
+  // Code for both global and local version here…
+  log.info("core-cli", "Code for both global and local version here…")
+  entry(process.argv.slice(2))
+}
