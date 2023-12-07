6d6e83116755999449ec7d374172f1aca7499dbe
兰亭古墨 <2443992009@qq.com>
2023-12-07 13:48:12 +0800

    Update release.config.cjs

--- release.config.cjs
+++ release.config.cjs
@@ -5,4 +5,5 @@ module.exports = {
   repositoryUrl: "https://github.com/HeFeng-Lab/core-cli.git",
   branches: ["main"],
   plugins: ["@semantic-release/commit-analyzer", "@semantic-release/release-notes-generator", ["@semantic-release/npm", { npmPublish: true }], "@semantic-release/github"],
+  debug: true,
 }
