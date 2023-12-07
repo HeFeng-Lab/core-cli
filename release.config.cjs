0cdb7758d4b905cb1b53e263aeeaff931765a6f2
hefeng6500 <2443992009@qq.com>
2023-12-07 10:36:37 +0800

    fix: release plugins

--- release.config.cjs
+++ release.config.cjs
@@ -3,4 +3,5 @@
  */
 module.exports = {
   branches: ["main", "next"],
+  plugins: ["@semantic-release/commit-analyzer", "@semantic-release/release-notes-generator", ["@semantic-release/npm", { npmPublish: true }], "@semantic-release/github"],
 }
